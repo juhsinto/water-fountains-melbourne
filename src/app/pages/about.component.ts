72ae796d2ee538fd6ddb875f57901ecea73826d5
Jacinto Mendes <jacinto.mendes@outlook.com>
2017-06-02 21:16:36 +1000

    all done

--- src/app/pages/about.component.ts
+++ src/app/pages/about.component.ts
@@ -6,9 +6,10 @@ import { Component } from '@angular/core';
     <h1> About </h1>
     <p> This app is made as a demo, to show off my skills in Angular!</p>
     <p> This app uses The <a href="http://getbootstrap.com/">Bootstrap</a> CSS framework, 
-      <a href="https://angular.io/">AngularJS (2)</a> 
+      <a href="https://angular.io/">AngularJS (2)</a> and the Public Water Fountains 
+      <a href="https://data.melbourne.vic.gov.au/Assets-Infrastructure/Drinking-fountains/h4ih-tzqs/data"> Dataset </a>
     </p>
-    <p> To see the source of this app, please visit <a href="https://github.com/juhsinto/Water">this</a> link.</p>
+    <p> To see the source of this app, please visit <a href="https://github.com/juhsinto/water-fountains-melbourne">this</a> link.</p>
   `
 })
 
